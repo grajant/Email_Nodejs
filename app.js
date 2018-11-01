e74cc6f29cd117fab4b0f060cd22e378144f4665
guillermo.grajales <guillermo.grajales@pragma.com.co>
2018-11-01 14:57:27 -0500

    McCain v2 added

--- app.js
+++ app.js
@@ -5,24 +5,37 @@ const EmailTemplate = require('email-templates');
 const emailParams = require('./config').emailCredentials;
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // true for 465, false for other ports
+    host: 'smtp.office365.com', // Office 365 server
+    port: 587,     // secure SMTP
+    secure: false, // false for TLS - as a boolean not string - but the default is false so just remove this completely
     auth: {
-        type: 'OAuth2',
-        user: emailParams.user,
-        clientId: emailParams.clientId,
-        clientSecret: emailParams.clientSecret,
-        refreshToken: emailParams.refreshToken,
-        accessToken: emailParams.accessToken,
+        user: 'contacto@souschefmccain.com.co',
+        pass: 'contacto2018$'
+    },
+    tls: {
+        ciphers: 'SSLv3'
     }
+    /*host: 'smtp.pragma.com.co',
+    port: 25*/
+// host: 'smtp.gmail.com',
+// port: 465,
+// secure: true, // true for 465, false for other ports
+// auth: {
+//     type: 'OAuth2',
+//     user: emailParams.user,
+//     clientId: emailParams.clientId,
+//     clientSecret: emailParams.clientSecret,
+//     refreshToken: emailParams.refreshToken,
+//     accessToken: emailParams.accessToken,
+// }
 });
 
 const email = new EmailTemplate({
     message: {
-        from: 'Cápsulas Express Nutresa <capsulasexpressnutresa@pragma.com.co>'
+        from: 'Sous Chef McCain <contacto@souschefmccain.com.co>'
+        //from: 'Capsulas <capsulasexpressnutresa@pragma.com.co>'
     },
-    send: true,
+    send: false,    // True to send real email
     transport: transporter,
     views: {
         options: {
@@ -32,16 +45,28 @@ const email = new EmailTemplate({
 });
 
 const sendEmail = async () => {
+    const tempArray = ['Guillermo', 'Guillermo'];
+    const emails = ['guillo.grajales@gmail.com', 'glego40_@hotmail.com'];
     try {
+        /*for (let i = 0; i < tempArray.length; i++) {
+            const result = await email.send({
+                template: 'mccain',
+                message: {
+                    to: emails[i]
+                },
+                locals: {
+                    firstName: tempArray[i]
+                }
+            });
+            console.log(result);
+        }*/
         const result = await email.send({
-            template: 'welcome',
+            template: 'mccain',
             message: {
-                to: ['glego40_@hotmail.com', 'guillo.grajales@gmail.com', 'guillo.grajales@yahoo.com']
-                //to: 'glego40_@hotmail.com'
-                //to: 'guillo.grajales@yahoo.com'
+                to: 'guillermo.grajales@pragma.com.co'
             },
             locals: {
-                firstName: 'Guillermo'
+                repName: 'Guillermo'
             }
         });
         console.log(result);
