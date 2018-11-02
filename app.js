f1053635780de9d463290fd2627ad586f2487f3a
grajant <glego40_@hotmail.com>
2018-11-02 00:56:45 -0500

    McCain email v2 implemented

--- app.js
+++ app.js
@@ -5,37 +5,37 @@ const EmailTemplate = require('email-templates');
 const emailParams = require('./config').emailCredentials;
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.office365.com', // Office 365 server
-    port: 587,     // secure SMTP
-    secure: false, // false for TLS - as a boolean not string - but the default is false so just remove this completely
-    auth: {
-        user: 'contacto@souschefmccain.com.co',
-        pass: 'contacto2018$'
-    },
-    tls: {
-        ciphers: 'SSLv3'
-    }
+    // host: 'smtp.office365.com', // Office 365 server
+    // port: 587,     // secure SMTP
+    // secure: false, // false for TLS - as a boolean not string - but the default is false so just remove this completely
+    // auth: {
+    //     user: 'contacto@souschefmccain.com.co',
+    //     pass: 'contacto2018$'
+    // },
+    // tls: {
+    //     ciphers: 'SSLv3'
+    // }
     /*host: 'smtp.pragma.com.co',
     port: 25*/
-// host: 'smtp.gmail.com',
-// port: 465,
-// secure: true, // true for 465, false for other ports
-// auth: {
-//     type: 'OAuth2',
-//     user: emailParams.user,
-//     clientId: emailParams.clientId,
-//     clientSecret: emailParams.clientSecret,
-//     refreshToken: emailParams.refreshToken,
-//     accessToken: emailParams.accessToken,
-// }
+host: 'smtp.gmail.com',
+port: 465,
+secure: true, // true for 465, false for other ports
+auth: {
+    type: 'OAuth2',
+    user: emailParams.user,
+    clientId: emailParams.clientId,
+    clientSecret: emailParams.clientSecret,
+    refreshToken: emailParams.refreshToken,
+    accessToken: emailParams.accessToken,
+}
 });
 
 const email = new EmailTemplate({
     message: {
-        from: 'Sous Chef McCain <contacto@souschefmccain.com.co>'
-        //from: 'Capsulas <capsulasexpressnutresa@pragma.com.co>'
+        //from: 'Sous Chef McCain <contacto@souschefmccain.com.co>'
+        from: 'Capsulas <capsulasexpressnutresa@pragma.com.co>'
     },
-    send: false,    // True to send real email
+    send: true,    // True to send real email
     transport: transporter,
     views: {
         options: {
@@ -61,12 +61,14 @@ const sendEmail = async () => {
             console.log(result);
         }*/
         const result = await email.send({
-            template: 'mccain',
+            template: 'mccain_v2',
             message: {
-                to: 'guillermo.grajales@pragma.com.co'
+                to: ['guillo.grajales@gmail.com', 'glego40_@hotmail.com']
             },
             locals: {
-                repName: 'Guillermo'
+                repName: 'Guillermo',
+                username: 'glego40_@hotmail.com',
+                password: 'N)_87jlke'
             }
         });
         console.log(result);
